255d835dfccc246f681618a2d99fb665c07249f0
nicknameismos <mynameissarawut@gmail.com>
2017-08-31 18:17:11 +0700

    update

--- modules/products/server/controllers/products.server.controller.js
+++ modules/products/server/controllers/products.server.controller.js
@@ -4,92 +4,93 @@
  * Module dependencies.
  */
 var path = require('path'),
-  mongoose = require('mongoose'),
-  Product = mongoose.model('Product'),
-  errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller')),
-  _ = require('lodash');
+    mongoose = require('mongoose'),
+    Product = mongoose.model('Product'),
+    errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller')),
+    _ = require('lodash');
 
 /**
  * Create a Product
  */
 exports.create = function(req, res) {
-  var product = new Product(req.body);
-  product.user = req.user;
+    var product = new Product(req.body);
+    product.user = req.user;
 
-  product.save(function(err) {
-    if (err) {
-      return res.status(400).send({
-        message: errorHandler.getErrorMessage(err)
-      });
-    } else {
-      res.jsonp(product);
-    }
-  });
+    product.save(function(err) {
+        if (err) {
+            return res.status(400).send({
+                message: errorHandler.getErrorMessage(err)
+            });
+        } else {
+            res.jsonp(product);
+        }
+    });
 };
 
 /**
  * Show the current Product
  */
 exports.read = function(req, res) {
-  // convert mongoose document to JSON
-  var product = req.product ? req.product.toJSON() : {};
+    // convert mongoose document to JSON
+    var product = req.product ? req.product.toJSON() : {};
 
-  // Add a custom field to the Article, for determining if the current User is the "owner".
-  // NOTE: This field is NOT persisted to the database, since it doesn't exist in the Article model.
-  product.isCurrentUserOwner = req.user && product.user && product.user._id.toString() === req.user._id.toString();
+    // Add a custom field to the Article, for determining if the current User is the "owner".
+    // NOTE: This field is NOT persisted to the database, since it doesn't exist in the Article model.
+    product.isCurrentUserOwner = req.user && product.user && product.user._id.toString() === req.user._id.toString();
 
-  res.jsonp(product);
+    res.jsonp(product);
 };
 
 /**
  * Update a Product
  */
 exports.update = function(req, res) {
-  var product = req.product;
+    var product = req.product;
 
-  product = _.extend(product, req.body);
+    product = _.extend(product, req.body);
 
-  product.save(function(err) {
-    if (err) {
-      return res.status(400).send({
-        message: errorHandler.getErrorMessage(err)
-      });
-    } else {
-      res.jsonp(product);
-    }
-  });
+    product.save(function(err) {
+        if (err) {
+            return res.status(400).send({
+                message: errorHandler.getErrorMessage(err)
+            });
+        } else {
+            res.jsonp(product);
+        }
+    });
 };
 
 /**
  * Delete an Product
  */
 exports.delete = function(req, res) {
-  var product = req.product;
+    var product = req.product;
 
-  product.remove(function(err) {
-    if (err) {
-      return res.status(400).send({
-        message: errorHandler.getErrorMessage(err)
-      });
-    } else {
-      res.jsonp(product);
-    }
-  });
+    product.remove(function(err) {
+        if (err) {
+            return res.status(400).send({
+                message: errorHandler.getErrorMessage(err)
+            });
+        } else {
+            res.jsonp(product);
+        }
+    });
 };
 
 /**
  * List of Products
  */
 exports.list = function(req, res) {
-  Product.find().sort('-created').populate('user', 'displayName').exec(function(err, products) {
-    if (err) {
-      return res.status(400).send({
-        message: errorHandler.getErrorMessage(err)
-      });
-    } else {
-      res.jsonp(products);
-    }
-  });
+    Product.find().sort('-created').populate('user', 'displayName').populate('shopseller').exec(function(err, products) {
+        console.log(products);
+        if (err) {
+            return res.status(400).send({
+                message: errorHandler.getErrorMessage(err)
+            });
+        } else {
+            res.jsonp(products);
+        }
+    });
 };
 
 /**
@@ -97,21 +98,21 @@ exports.list = function(req, res) {
  */
 exports.productByID = function(req, res, next, id) {
 
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(400).send({
-      message: 'Product is invalid'
-    });
-  }
-
-  Product.findById(id).populate('user', 'displayName').exec(function (err, product) {
-    if (err) {
-      return next(err);
-    } else if (!product) {
-      return res.status(404).send({
-        message: 'No Product with that identifier has been found'
-      });
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).send({
+            message: 'Product is invalid'
+        });
     }
-    req.product = product;
-    next();
-  });
+
+    Product.findById(id).populate('user', 'displayName').exec(function(err, product) {
+        if (err) {
+            return next(err);
+        } else if (!product) {
+            return res.status(404).send({
+                message: 'No Product with that identifier has been found'
+            });
+        }
+        req.product = product;
+        next();
+    });
 };
